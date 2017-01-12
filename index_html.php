3edc472b43cc2e7cf8f02a09bd8d55069b7a2cd6
JaSoul <young10270@gmail.com>
2017-01-12 16:22:16 +0900

    bootstrap

--- index_html.php
+++ index_html.php
@@ -10,8 +10,13 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
   <!--Css-->
   <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+  <link href="http://localhost/bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
 	<header>
@@ -48,7 +53,10 @@
       }
     ?>
   </article>
-
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="http://localhost/bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
 
