d70789c6543346cb15d73bb53238e8297b4f8b21
JaSoul <young10270@gmail.com>
2017-01-12 18:05:57 +0900

    bootstrap

--- index_html.php
+++ index_html.php
@@ -13,46 +13,54 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
   <!--Css-->
-  <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+  <link rel="stylesheet" type="text/css" href="http://localhost/style_BS.css">
   <link href="http://localhost/bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
-	<header>
-    <img src="http://ddolking.tv/layouts/Dk/img/title.png" alt="왕관">
-		<h1><a href = "http://localhost/index_html.php">JavaScript</a></h1>
-	</header>
-	<nav>
-	<ul>
-  <?php
-    //
-    while($row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost/index_html.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
-    }
-  ?>
-	</ul>
-	</nav>
-
-	<div id="control">
-		<input type="button" value="white" onclick="document.getElementById('target').className='white'">
-		<input type="button" value="black" onclick="document.getElementById('target').className='black'">
-    <a href="http://localhost/write.php">쓰기</a>
+  <div class="container">
+  	<header class="jumbotron">
+      <img src="http://ddolking.tv/layouts/Dk/img/title.png" alt="왕관" id="logo">
+  		<h1 class="text-center"><a href = "http://localhost/index_html.php">JavaScript</a></h1>
+  	</header>
+    <!--bootstrap grid-->
+    <div class="row">
+      <nav class="col-md-2">
+    	<ul class="nav nav-pills nav-stacked">
+      <?php
+        while($row = mysqli_fetch_assoc($result)){
+          echo '<li><a href="http://localhost/index_html.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+        }
+      ?>
+    	</ul>
+    	</nav>
+    	<div class="col-md-10">
+        <article>
+          <?php
+            if(empty($_GET['id'])==false){
+              $sql= 'SELECT topic.id,title,name,description from topic left join user on topic.author=user.id where topic.id='.$_GET['id'];
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_assoc($result);
+              //user input security
+              echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+              echo '<p>'.htmlspecialchars($row['name']).'</p>';
+              //allow specified tags / deny excluded tags
+              echo strip_tags($row['description'],'<a><h1><h2><h3><h4><h5><h6><ol><ul><li>');
+            }
+          ?>
+        </article>
+        <!--horizontal line-->
+        <hr>
+        <div id="control">
+          <div class="btn-group">
+            <input class="btn btn-default" type="button" value="white" onclick="document.getElementById('target').className='white'">
+            <input class="btn btn-default" type="button" value="black" onclick="document.getElementById('target').className='black'">
+          </div>
+          <a class="btn btn-success" href="http://localhost/write.php">쓰기</a>
+        </div>
+    	</div>
+    </div>
   </div>
-  <article>
-    <?php
-      if(empty($_GET['id'])==false){
-        $sql= 'SELECT topic.id,title,name,description from topic left join user on topic.author=user.id where topic.id='.$_GET['id'];
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        //user input security
-        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-        echo '<p>'.htmlspecialchars($row['name']).'</p>';
-        //allow specified tags / deny excluded tags
-        echo strip_tags($row['description'],'<a><h1><h2><h3><h4><h5><h6><ol><ul><li>');
-      }
-    ?>
-  </article>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
--- write.php
+++ write.php
@@ -1,6 +1,8 @@
 <?php
-  require("lib/db/php");
+  //library -> repetition removal
+  require("lib/db.php");
   require("config/config.php");
+  //DB conn
   $conn=db_init($config["host"],$config["duser"],$config["dpw"],$config["dname"]);
   $result = mysqli_query($conn,"SELECT * FROM topic");
 ?>
@@ -8,57 +10,70 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
-</head>
-<body id="target">
-	<header>
-    <img src="http://ddolking.tv/layouts/Dk/img/title.png" alt="왕관">
-		<h1><a href = "http://localhost/index_html.php">JavaScript</a></h1>
-	</header>
-	<nav>
-	<ul>
-  <?php
-    while($row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost/index_html.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-    }
-  ?>
-	</ul>
-	</nav>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <!--Css-->
+  <link rel="stylesheet" type="text/css" href="http://localhost/style_BS.css">
+  <link href="http://localhost/bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 
-	<div id="control">
-		<input type="button" value="white" onclick="document.getElementById('target').className='white'">
-		<input type="button" value="black" onclick="document.getElementById('target').className='black'">
-    <a href="http://localhost/write.php">쓰기</a>
-  </div>
-  <article>
-    <form action="process.php" method="post">
-      <p>
-        제목 : <input type="text" name="title">
-      </p>
-      <p>작성자 : <input type="text" name="author">
-      </p>
-      <p>
-        본문 : <textarea name="description"></textarea>
-      </p>
-      <input type="hidden" role="uploadcare-uploader" />
-      <input type="submit" name="name">
-    </form>
-  </article>
   <script>
     UPLOADCARE_PUBLIC_KEY = "a5e307020d58f5703c31";
   </script>
   <script charset="utf-8" src="//ucarecdn.com/libs/widget/2.10.2/uploadcare.full.min.js"></script>
+</head>
+<body id="target">
+  <div class="container">
+  	<header class="jumbotron">
+      <img src="http://ddolking.tv/layouts/Dk/img/title.png" alt="왕관" id="logo">
+  		<h1 class="text-center"><a href = "http://localhost/index_html.php">JavaScript</a></h1>
+  	</header>
+    <!--bootstrap grid-->
+    <div class="row">
+      <nav class="col-md-2">
+    	<ul class="nav nav-pills nav-stacked">
+      <?php
+        while($row = mysqli_fetch_assoc($result)){
+          echo '<li><a href="http://localhost/index_html.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+        }
+      ?>
+    	</ul>
+    	</nav>
+    	<div class="col-md-10">
+        <article>
+          <form action="process.php" method="post">
+            <div class="form-group">
+              <label for="form-title"> Title </label>
+              <input type="text" class="form-control" name="title" id="form-title" placeholder="제목을 적으세요.">
+            </div>
+            <div class="form-group">
+              <label for="form-author"> Author </label>
+              <input type="text" class="form-control" name="author" id="form-author" placeholder="작성자를 적으세요.">
+            </div>
+            <div class="form-group">
+              <label for="form-description"> Description </label>
+              <textarea type="text" class="form-control" rows="10" name="description" id="form-description" placeholder="본문을 작성하세요."></textarea>
+            </div>
+
+            <input type="hidden" role="uploadcare-uploader" />
+            <input class="btn btn-default" type="submit" name="name" id="submit">
+          </form>
+        </article>
+    	</div>
+    </div>
+  </div>
 
   <script>
-    //role의 값이 uploadcare-uploader인 태그를 업로드 위젯으로 만들어라.
     var singleWidget = uploadcare.singleWidget('[role=uploadcare-uploader]');
-    //그 위젯을 통해서 업로드가 끝났을 떄
-    singleWidget.onUploadComplete(function(info){
-      //id 값이 description인 태그의 값 뒤에 업로드한 이미지 파일의 주소를 이미지 태그와 함께 첨부해라.
-      document.getElementById('description').value = document.getElementById('description').value + '<img src ="'+info.cdnUrl+'">';
+    singleWidget.onUPloadComplete(function(info){
+      document.getElementById('form-description').value = document.getElementById('form-description').value + '<img crc="'+info.cdnUrl+'">';
     });
   </script>
 
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="http://localhost/bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
 
